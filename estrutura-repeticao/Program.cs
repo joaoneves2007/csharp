b21879221bfafc900c00f625bbb701d7d5cb8560
Arthur-Winnicios <contatojotaico@gmail.com>
2025-02-19 07:58:09 -0400

    add estrutura repeticao

---
+++ estrutura-repeticao/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1,101);
+int contador = 0;
+Console.WriteLine($"Número Alvo:{numero}");
+while (contador <= numero)
+{
+    Console.WriteLine($"Contador:{contador}.");
+    contador++;
+}
+Console.WriteLine($"Contador atingiu o número alvo:{contador}");
+int numero = 0;
+
+do
+{
+    Console.WriteLine($"Número:{numero}");
+    numero++;
+}while (numero < 10);
