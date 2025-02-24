466544926419e139b24a821c0628f18f28442866
Arthur-Winnicios <contatojotaico@gmail.com>
2025-02-24 11:26:25 -0400

    add exercicios array

---
+++ estruturas-dados/Program.cs
@@ -0,0 +1,35 @@
+﻿//Estrutura de dados array
+int[] meuArray = {0,1,2,3,4,5};
+
+foreach (int num in meuArray)
+{
+    Console.WriteLine(num);
+}
+//Estrutura de dados lista
+List<string> listaNomes = new List<string>{"Wanderson", "Jeane", "Ryan"};
+listaNomes.Add("Maria");
+
+foreach (string nome in listaNomes)
+{
+    Console.WriteLine(nome);
+}
+Console.WriteLine("");
+//remover
+
+listaNomes.RemoveAt(0);
+
+foreach (string nome in listaNomes)
+{
+    Console.WriteLine(nome);
+}
+Console.WriteLine("");
+
+//Estrutura de dados conjunto
+HashSet<int> conjunto= new HashSet<int>{1,2,3};
+conjunto.Add(4);
+Console.WriteLine("");
+
+foreach (int num in conjunto)
+{
+    Console.WriteLine(num);
+}
---
+++ exercicio-array/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numeros = { 1, 2, 3, 4, 5 };
+int soma = numeros.Sum();
+{
+    Console.WriteLine($"Soma dos elementos: {soma}");
+}
+List<string> nomes = new List<string> { "Jeane", "Ryan", "Jeane" };
+int contagem = nomes.Count(n => n.Equals("Jeane", StringComparison.OrdinalIgnoreCase));
+{
+    Console.WriteLine($"O nome Jeane aparece {contagem} vezes");
+}
+List<int> numeros = new List<int> { 1, 2, 2, 3, 4, 5 };
+List<int> semDuplicatas = numeros.Distinct().ToList();
+{
+    Console.WriteLine("Lista sem duplicatas: " + string.Join(", ", semDuplicatas));
+}
+HashSet<string> frutas = new HashSet<string> { "Maçã", "Banana", "Laranja" };
+{
+    Console.WriteLine(frutas.Contains("Banana") ? "Banana está no conjunto" : "Banana não encontrada");
+}
+List<int> numeros = new List<int>();
+for (int i = 1; i <= 5; i++)
+    numeros.Add(i);
+{
+    Console.WriteLine("Elementos da lista: " + string.Join(", ", numeros));
+}
